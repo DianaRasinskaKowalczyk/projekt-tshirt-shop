75dc93530148d1310c625377c44d820e24160b56
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2022-07-07 15:46:27 +0200

    Add new components

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,47 +1,52 @@
 import styles from './Product.module.scss';
-import ProductForm from '../ProductForm/ProductForm';
+import OptionForm from '../OptionForm/OptionForm';
+import ProductImage from '../ProductImage/ProductImage';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 
-const Product = ({title, colors, sizes, name, basePrice }) => {
+
+const Product = ({name, colors, sizes, title, basePrice }) => {
 
   const [currentColor, setCurrentColor] = useState(colors[0]);
-  const [currentSize, setCurrentSize] = useState(sizes[0].name);
+  const [currentSize, setCurrentSize] = useState(sizes[0]);
 
-  const handleSubmit = e => {
+  const getPrice = useMemo(() => {
+    return (
+      basePrice +
+      sizes.find((element) => element === currentSize).additionalPrice
+    );
+  }, [currentSize, basePrice, sizes]);
+
+  const clickHandler = e => {
     e.preventDefault();
- 
-  };
+    console.log('Summary');
+    console.log('Name:', name);
+    console.log('Price:', getPrice);
+    console.log('Size:', currentSize.name);
+    console.log('Color:', currentColor);
+  }
 
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
-          className={styles.image}
-          alt= {title}
-          src={`${process.env.PUBLIC_URL}/images/products/${name}--${currentColor}.jpg`} />
+        <ProductImage name={name} currentColor={currentColor} title={title} />
       </div>
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
-          <span className={styles.price}>Price: {basePrice}$</span>
+          <span className={styles.price}>Price: {getPrice}$</span>
         </header>
-      <ProductForm 
-        colors={colors} 
-        setCurrentColor={setCurrentColor}
-        currentColor={currentColor}
-        sizes={sizes}
-        setCurrentSize={setCurrentSize}
-        currentSize={currentSize}
-        handleSubmit={handleSubmit} />
+        <OptionForm colors={colors} sizes={sizes} currentSize={currentSize} currentColor={currentColor} setCurrentSize={setCurrentSize} setCurrentColor={setCurrentColor} clickHandler={clickHandler} />
       </div>
     </article>
   )
 };
 
+
 Product.propTypes= {
   basePrice: PropTypes.number.isRequired,
   colors: PropTypes.array.isRequired,
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   sizes: PropTypes.array.isRequired,
   title: PropTypes.string.isRequired,
